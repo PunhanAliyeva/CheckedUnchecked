c09e24b922b7d415dbf83eae97f96b3ddb0a5ced
PunhanAliyeva <eliyevapunhan567@gmail.com>
2024-03-17 17:25:46 +0400

    Add project files.

---
+++ CheckedUnchecked/Program.cs
@@ -0,0 +1,68 @@
+﻿
+namespace CheckedUnchecked
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Task1
+            //IsPrime(1);
+            #endregion
+
+
+            #region Task2
+            //double result = CalcAvg(34, 68, 45, 90, 21, 74, 88);
+            //if (result > 60)
+            //{
+            //    Console.WriteLine("Mezun oldunuz..");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Mezun ola bilmediniz..");
+            //}
+            #endregion
+
+        }
+
+        #region Task1
+        //static void IsPrime(int value)
+        //{
+        //    int count = 0;
+        //    bool CheckPrime = true;
+        //    if (value == 1)
+        //    {
+        //        Console.WriteLine("Neither prime nor composite..");
+        //        return;
+        //    }
+        //    for (int i = 1; i <= value; i++)
+        //    {
+        //        if (value % i == 0)
+        //        {
+        //            count++;
+        //        }
+        //        if (count > 2)
+        //        {
+        //            CheckPrime = false;
+        //            break;
+        //        }
+        //    }
+        //    Console.WriteLine(CheckPrime);
+        //}
+        #endregion
+
+
+        #region Task2
+        //static int CalcAvg(params int[] marks)
+        //{
+        //    int sum = 0;
+        //    for (int i = 0; i < marks.Length; i++)
+        //    {
+        //        sum += marks[i];
+        //    }
+        //    return sum / marks.Length;
+        //}
+        #endregion
+
+    }
+}
